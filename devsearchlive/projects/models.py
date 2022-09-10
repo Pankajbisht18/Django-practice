48d672ac6d6f7bec2b0be4fa1f69d19c6e7558d0
Pankajbisht18 <techpankaj45@gmail.com>
2022-09-10 23:48:22 +0530

    Add models

--- devsearchlive/projects/models.py
+++ devsearchlive/projects/models.py
@@ -1,3 +1,17 @@
+from operator import mod
+import uuid
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=1000, null=True, blank=True)
+    source_link = models.CharField(max_length=1000, null=True, blank=True)
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid1, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
