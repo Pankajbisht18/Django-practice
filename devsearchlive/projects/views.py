3a45bb581712460d7dae14dc13d8cd2f908d6032
Pankajbisht18 <techpankaj45@gmail.com>
2022-09-21 00:27:27 +0530

    Add django form

--- devsearchlive/projects/views.py
+++ devsearchlive/projects/views.py
@@ -1,28 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from .models import Project
-
-projectList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website',
-        'topRated': True
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website',
-        'topRated': False
-    },
-    {
-        'id': '1',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community',
-        'topRated': True
-    },
-
-]
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -37,3 +16,10 @@ def project(request, pk):
     reviews = projectObj.reviews.all()
     context = { 'project': projectObj, 'tags': tags, 'reviews': reviews }
     return render(request, 'projects/single-project.html', context)
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+
+    return render(request, 'projects/project-form.html', context)
